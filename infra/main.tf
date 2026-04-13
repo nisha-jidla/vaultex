ee1ed83af19adef42bb18fb1fafa00c3540f4280
nisha jidla <nishajidla@gmail.com>
2026-04-13 18:53:57 +0100

    Fix SSH key encoding

--- infra/main.tf
+++ infra/main.tf
@@ -63,8 +63,8 @@ resource "aws_security_group" "vaultex" {
 
   # HTTP
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
